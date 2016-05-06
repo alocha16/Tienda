d77e6ddd06d7960af732020fcb43bf1bfacf80b7
AlvaroLopez <alocha16@gmail.com>
2016-05-06 16:42:55 -0600

    Initial Commit

---
+++ Tienda/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tienda
+//
+//  Created by Alvaro Lopez on 6/5/16.
+//  Copyright © 2016 Alvaro Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
